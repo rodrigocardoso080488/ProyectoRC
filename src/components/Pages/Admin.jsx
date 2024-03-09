37ed5cfcb07b7235c6298a5594551e60dbc5ed75
rodrigocardoso <rodrigocardoso080488@gmail.com>
2024-03-09 08:22:33 -0300

    se completo la creación del producto

--- src/components/Pages/Admin.jsx
+++ src/components/Pages/Admin.jsx
@@ -1,8 +1,17 @@
+import ListadoProductos from '../Section/ListadoProductos';
+import { Button } from "react-bootstrap";
+
+import {useNavigate} from "react-router-dom";
+//UseNavigate funciona de la misma forma que funciona window.location.replace. Siempre que queramos redirigir vamos a utilizar los métodos que nos proporciona react-router-dom. Por convención se crea una constante llamada navigate y se lo iguala al método useNavigate().
 
 const Administracion = () => {
+    const navigate = useNavigate();
     return (
         <div>
-            <h1>Desde Admin</h1>
+            <div className='container my-3 py-3'>
+                <Button variant="primary" onClick={()=>{navigate("/crear-producto")}}>Crear Producto</Button>
+            </div>
+            <ListadoProductos/>
         </div>
     );
 };
---
+++ src/components/Section/ListadoProductos.jsx
@@ -0,0 +1,11 @@
+
+
+const ListadoProductos = () => {
+    return (
+        <div>
+            <h1>Listado Productos</h1>
+        </div>
+    );
+};
+
+export default ListadoProductos;
